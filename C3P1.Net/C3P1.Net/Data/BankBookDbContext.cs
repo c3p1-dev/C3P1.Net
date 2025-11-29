c39298c497c905a564de5b6f6f2fc1cd14707ebd
c3p1-dev <mat.clot@gmail.com>
2025-11-29 21:32:28 +0100

    Code Cleaning

--- C3P1.Net/C3P1.Net/Data/BankBookDbContext.cs
+++ C3P1.Net/C3P1.Net/Data/BankBookDbContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
-using C3P1.Net.Client.Data.Apps.BankBook;
+﻿using C3P1.Net.Client.Data.Apps.BankBook;
+using Microsoft.EntityFrameworkCore;
 
 namespace C3P1.Net.Data
 {
-    public class BankBookDbContext (DbContextOptions<BankBookDbContext> options) : DbContext(options)
+    public class BankBookDbContext(DbContextOptions<BankBookDbContext> options) : DbContext(options)
     {
         public DbSet<BankAccount> BankAccounts => Set<BankAccount>();
     }
