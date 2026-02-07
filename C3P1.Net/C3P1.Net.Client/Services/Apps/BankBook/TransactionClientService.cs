3eaa93377a3dded30c39b6b6682d36b4d5897df9
c3p1-dev <mat.clot@gmail.com>
2026-02-07 18:58:28 +0100

    Some progress
    Add LedgerPage
    Upgrade to Blazorise 1.8.10
    Correct TransactionClientService (endpoint issues)
    Create TransactionLedgerGridDto

--- C3P1.Net/C3P1.Net.Client/Services/Apps/BankBook/TransactionClientService.cs
+++ C3P1.Net/C3P1.Net.Client/Services/Apps/BankBook/TransactionClientService.cs
@@ -8,49 +8,49 @@ namespace C3P1.Net.Client.Services.Apps.BankBook
     {
         public async Task<bool> AddTransactionAsync(Guid userId, Transaction transaction)
         {
-            var result = await httpClient.PostAsJsonAsync($"/api/bankbook/transaction/add", transaction);
+            var result = await httpClient.PostAsJsonAsync($"/api/apps/bankbook/transaction/add", transaction);
 
             return result.IsSuccessStatusCode;
         }
 
         public async Task<bool> DeleteTransactionAsync(Guid userId, Guid transactionId)
         {
-            var result = await httpClient.DeleteAsync($"/api/bankbook/transaction/delete/{transactionId}");
+            var result = await httpClient.DeleteAsync($"/api/apps/bankbook/transaction/delete/{transactionId}");
 
             return result.IsSuccessStatusCode;
         }
 
         public async Task<Transaction?> GetTransactionByIdAsync(Guid userId, Guid transactionId)
         {
-            var result = await httpClient.GetFromJsonAsync<Transaction?>($"/api/bankbook/transaction/get/{transactionId}");
+            var result = await httpClient.GetFromJsonAsync<Transaction?>($"/api/apps/bankbook/transaction/get/{transactionId}");
 
             return result;
         }
 
         public async Task<List<Transaction>> GetTransactionsAsync(Guid userId)
         {
-            var result = await httpClient.GetFromJsonAsync<List<Transaction>>($"/api/bankbook/transaction/list");
+            var result = await httpClient.GetFromJsonAsync<List<Transaction>>($"/api/apps/bankbook/transaction/list");
 
             return result!;
         }
 
         public async Task<List<Transaction>> GetTransactionsByAccountIdAsync(Guid userId, Guid accountId)
         {
-            var result = await httpClient.GetFromJsonAsync<List<Transaction>>($"/api/banobook/transaction/list/account/{accountId}");
+            var result = await httpClient.GetFromJsonAsync<List<Transaction>>($"/api/apps/bankbook/transaction/list/account/{accountId}");
 
             return result!;
         }
 
         public async Task<List<Transaction>> GetTransactionsBySubCategoryIdAsync(Guid userId, Guid subCategoryId)
         {
-            var result = await httpClient.GetFromJsonAsync<List<Transaction>>($"/api/bankbook/transaction/list/subcategory/{subCategoryId}");
+            var result = await httpClient.GetFromJsonAsync<List<Transaction>>($"/api/apps/bankbook/transaction/list/subcategory/{subCategoryId}");
 
             return result!;
         }
 
         public async Task<bool> UpdateTransactionAsync(Guid userId, Transaction transaction)
         {
-            var result = await httpClient.PutAsJsonAsync($"/api/bankbook/transaction/update", transaction);
+            var result = await httpClient.PutAsJsonAsync($"/api/apps/bankbook/transaction/update", transaction);
 
             return result.IsSuccessStatusCode;
         }
---
+++ C3P1.Net/C3P1.Net.Shared/Data/Apps/BankBook/Dto/TransactionLedgerGridDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace C3P1.Net.Shared.Data.Apps.BankBook.Dto
+{
+    public class TransactionGridDto
+    {
+        [Required]
+        public DateOnly AccountingDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+        [Required]
+        [StringLength(150)]
+        public string Label { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(10, ErrorMessage = "SubCategory Code is at most 10 characters")]
+        [RegularExpression("^[A-Z0-9_\\-!?@€$%]{1,10}$", ErrorMessage = "Invalid format : A-Z and 0-9, !?@€$% only, at most 10 characters")]
+        public string SubCategoryCode { get; set; } = string.Empty;
+
+        public string CategoryCode { get; set; } = string.Empty;
+        public PaymentMethod PaymentMethod { get; set; }
+        public string? CheckNumber { get; set; }
+        public string? Note { get; set; }
+        public DateOnly? ValueDate { get; set; } = DateOnly.FromDateTime(DateTime.Today); // +/- .AddDay(x)
+
+        [Required]
+        [CustomValidation(typeof(TransactionGridDto), nameof(ValidateAmount))]
+        public decimal Amount { get; set; }
+
+        public bool IsReconciled { get; set; } = false;
+
+        public static ValidationResult? ValidateAmount(decimal amount, ValidationContext context)
+        {
+            if (amount == 0)
+                return new ValidationResult("Amount cannot be zero");
+
+            return ValidationResult.Success;
+        }
+
+    }
+}
